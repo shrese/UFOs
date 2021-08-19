39c7f8918eada929b468b8dd8183897118c482ba
shrese rudd <srudd1@hotmail.com>
2021-08-18 18:08:42 -0600

    adding 10.6.1 code

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
   // first, clear out any exising data
   tbody.html("");
@@ -22,17 +28,17 @@ function handleclick () {
   let date = d3.select("#datetime").property("value");
   let filteredData = tableData;
   // check to see if a date was selected and filter the data using that date
-  if (date) ={
+  if (date) {
     // apply a filter to the table data to only keep the rows where the date time value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
   // rebuild the the table using the filtered data
   // if no date was entered, then filted data will be the original table
   buildTable(filteredData);
-
+  }
   // attach an event to listen for the form button
   d3.selectAll("#filter-btn").on("click", handleclick);
 
   // build the table when the page loads
   buildTable(buildTable);
-  };
+  
